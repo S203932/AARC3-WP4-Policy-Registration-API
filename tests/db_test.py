2f200df2d000e3d19e72f4a0978f8bd257cd4132
Patrick Machalet <patrick.machalet@cern.ch>
2025-09-24 14:33:35 +0200

    Maybe??

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,22 +1,9 @@
 import pytest
 import requests
 import os
-from mysql.connector import pooling
 
-db_config = {
-    "host": os.environ.get("DB_HOST"),
-    "user": os.environ.get("DB_USER"),
-    "password": os.environ.get("DB_PASSWORD"),
-    "port": int(os.environ.get("DB_PORT")),
-    "database": os.environ.get("DB_NAME"),
-}
 
 baseUrl = os.environ.get("APP_URL")
-
-connection_pool = pooling.MySQLConnectionPool(
-    pool_name="mypool", pool_size=4, **db_config
-)
-
 
 
 def testLandingPage():
