69617714cdfe06257fdbc90f640ca3312ba67a85
Patrick Machalet <patrick.machalet@cern.ch>
2025-09-25 13:56:21 +0200

    Checking if the test can easily reach the mysql given it isn't in seperate container

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,12 +1,41 @@
 import pytest
 import requests
 import os
+import mysql.connector
 
 
-baseUrl = os.environ.get("APP_URL")
+
+db_config = {
+    "host": os.environ.get("DB_HOST"),
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "port": int(os.environ.get("DB_PORT")),
+    "database": os.environ.get("DB_NAME")
+}
+
+
+db = mysql.connector.connect(
+    host = os.environ.get("DB_HOST"),
+    user = os.environ.get("DB_USER"),
+    password = os.environ.get("DB_PASSWORD"),
+    port = int(os.environ.get("DB_PORT")),
+    database = os.environ.get("DB_NAME")
+)
+
+
+cursor.execute("SHOW DATABASES")
+
+for x in cursor:
+  print(x) 
+
+
+""" baseUrl = os.environ.get("APP_URL")
 
 
 def testLandingPage():
     response = requests.get(baseUrl)
 
     assert response == "Hi, this is just a landing page"
+ """
+
+
