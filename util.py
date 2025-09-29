8d39a2e06e0dc26ca99290ccb7263057c9a8de26
Patrick Machalet <patrick.machalet@cern.ch>
2025-09-29 16:27:13 +0200

    Added software license GPLv3

--- util.py
+++ util.py
@@ -1,3 +1,10 @@
+'''
+Copyright (c) European Organization for Nuclear Research (CERN). 2025
+This file is part of AARC3-WP4-Policy-Registration-API.
+AARC3-WP4-Policy-Registration-API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+AARC3-WP4-Policy-Registration-API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with AARC3-WP4-Policy-Registration-API. If not, see <https://www.gnu.org/licenses/>. 
+'''
 import logging
 
 
